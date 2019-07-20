01a2b3e96a1e1ef883f10e43f30e75889466f74e
asm-jaime <asm.jaime@gmail.com>
2019-07-21 04:48:09 +1000

    aligning the database resource strucut

--- database/build.resource.js
+++ database/build.resource.js
@@ -1,67 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
-function get_ships() {
-  const db_ships = './ships';
+const get_resource = (resource) => {
+  const result = [];
+  const files = fs.readdirSync(resource);
 
-  const files = fs.readdirSync(db_ships);
-
-  //const res = Object.create(null);
-  const res = [];
   for(let i = 0; i < files.length; ++i) {
     if(path.extname(files[i]) === '.json') {
-      const rawdata = fs.readFileSync(`${db_ships}/${files[i]}`, 'utf8');
-      //res[path.basename(files[i], '.json')] = JSON.parse(rawdata);
-      res.push(JSON.parse(rawdata));
+      const rawdata = fs.readFileSync(`${resource}/${files[i]}`, 'utf8');
+      result.push(JSON.parse(rawdata));
     }
   }
 
-  return res;
-}
+  return result;
+};
 
-function get_parts() {
-  const db_parts = './parts';
-  const paths = [
-    { name: 'armaments', path: `${db_parts}/armaments.json` },
-    { name: 'gunports', path: `${db_parts}/gunports.json`},
-    { name: 'hulls', path: `${db_parts}/hulls.json`},
-    { name: 'panelings', path: `${db_parts}/panelings.json`},
-    { name: 'sails', path: `${db_parts}/sails.json`},
-  ];
-
-  return get_data(paths);
-}
-
-function get_data(paths) {
-  const res = Object.create(null);
-  for(let i = 0; i < paths.length; ++i){
-    const rawdata = fs.readFileSync(paths[i].path, 'utf8');
-    res[paths[i].name] = JSON.parse(rawdata)[paths[i].name];
-  }
-  return res;
-}
-
-function get_skills() {
-  const db_skills = './skills';
-  const paths = [
-    { name: 'skills_optional', path: `${db_skills}/skills-optional.json`},
-    { name: 'skills_original', path: `${db_skills}/skills-original.json`},
-  ];
-  return get_data(paths);
-}
-
-function get_resources() {
-  const res = Object.create(null);
-  res["ships"] = get_ships();
-  res["parts"] = get_parts();
-  res["skills"] = get_skills();
+const get_resources = (resources) => {
+  const result = resources.map(resource => {
+    const res = Object.create(null);
+    res[resource] = get_resource(`./${resource}`);
+    return res;
+  });
 
   fs.writeFile(
     '../public/resource.json',
-    JSON.stringify(res),
+    JSON.stringify(result),
     'utf8',
     () => { console.log('>>build resource.json complete'); }
   );
-}
+};
 
-get_resources();
+get_resources([
+  'armaments',
+  'gunports',
+  'hulls',
+  'panelings',
+  'sails',
+  'skills',
+  'skills_grade',
+  'ships'
+]);
