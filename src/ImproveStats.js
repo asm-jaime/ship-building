df587ff0ea526b6fd6b15ca3d479c731e9a5cf6b
asm-jaime <asm.jaime@gmail.com>
2019-08-07 11:14:18 +1000

    add the Improve stats component

---
+++ src/ImproveStats.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ImproveStats.css';
+
+import { get_iranges } from './StoreResolve';
+
+import {
+  IMPROVEABLE_PROPERTIES,
+  SHIP_STAT_NAMES
+} from './constants';
+
+const ImproveStats = (props) => {
+  const stats = get_iranges([props.step]);
+  return <div className='improve-stats'>{
+    IMPROVEABLE_PROPERTIES.map((p, i) => {
+      if(stats[i][0] !== 0 || stats[i][1] !== 0) {
+        return <div key={i} className='stat'>
+          <img src={`i_${p}.png`} alt={i}/>
+          {stats[i][0]}~{stats[i][1]}, {SHIP_STAT_NAMES[p]}
+        </div>;
+      } else {
+        return <div key={i}/>;
+      }
+    })
+  }</div>;
+};
+
+export default ImproveStats;
