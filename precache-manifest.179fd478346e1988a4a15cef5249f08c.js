592d56f505df613976316c1a707f6b0a202604d4
asm-jaime <asm.jaime@gmail.com>
2019-10-26 20:33:49 +1000

    ship-building/page/v0.1

---
+++ precache-manifest.179fd478346e1988a4a15cef5249f08c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489d7b2b2e5b4fc0d3b0e6900ec38651",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73b684bc2bb2656d86ac",
+    "url": "./static/css/main.bec75e0e.chunk.css"
+  },
+  {
+    "revision": "5ca0898b0d18df96d30c",
+    "url": "./static/js/2.c7837e63.chunk.js"
+  },
+  {
+    "revision": "73b684bc2bb2656d86ac",
+    "url": "./static/js/main.009d8c4b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
